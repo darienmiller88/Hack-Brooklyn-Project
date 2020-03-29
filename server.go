b5390aef214d71146da618ccab2860deaabc0245
darienmiller88 <darienmiller88@yahoo.com>
2020-03-29 17:18:12 -0400

    massive re-formating

--- server.go
+++ server.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
 	fmt.Println("Server is running")
+	// fs := http.FileServer(http.Dir("css"))
+	// http.Handle("/css/", http.StripPrefix("/css/", fs))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/about", aboutHandler)
+
+	//I hope this works :/...
+
 	http.ListenAndServe(":5000", nil)
 }
 
 func indexHandler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "<h1>landing page :O</h1>")
+	myTemplate, _ := template.ParseFiles("templates/example.html")
+	myTemplate.Execute(writer, request)
 }
 
 func aboutHandler(writer http.ResponseWriter, request *http.Request) {
